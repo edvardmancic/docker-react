f1a7036243e6b8c0ef045494c634065a1511e4e2
“edvardmancic” <edvardmancic@hotmail.com>
2021-01-05 21:38:23 +0100

    try

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Container CHECK update workflow!!
-        </p>
+        <h1>Anja Srecan ti rodjendan Tato</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
